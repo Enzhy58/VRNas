1dae8338b9cbe9ff60e0adafbf0f031eb669e36b
Enzhy58 <ang.borisova2015@yandex.ru>
2023-10-04 09:52:49 +0300

    added choose

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.accordion__header').on('click', function (e) {
+        e.preventDefault();
+        $('.accordion__header').removeClass('accordion__header--active');
+        $(this).addClass('accordion__header--active');
+
+        $('.accordion__body').removeClass('accordion__body--active');
+        $($(this).attr('href')).addClass('accordion__body--active');
+    });
+
+});
